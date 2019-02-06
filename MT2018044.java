59047cefaa523468037de2f562356a0d9ad041b8
jaydeepgodhani <jaydeepgodhani16@gmail.com>
2019-02-06 17:48:56 +0530

    initialize

---
+++ MT2018044.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class MT2018044
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        System.out.print("hi jaydeep");
+        System.out.print("hi godhani");
+        System.out.print("hi jd bhai");
+    }
+}
